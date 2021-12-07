9af086e9c6e53d2e39f0da3e329c2313623659d3
davidangarita1 <davidangarita.sistemas@gmail.com>
2021-12-07 13:29:09 -0500

    Se realiza test de validacion  evento, titilo y contenido basico

--- src/functions.test.js
+++ src/functions.test.js
@@ -1,11 +1,33 @@
 import { createEvent } from './functions'
 
+beforeAll(() => {
+    global.Date.now = jest.fn(() => new Date('2020-04-07T10:20:30Z').getTime())
+});
+
+const data = {
+    weekDay: 'mon',
+    week: 1,
+    openHour: 8,
+    closeHour: 14
+}
+
 test('Validation a event title and content basic', () => {
-    //TODO: hacer las verificaciones
+    //At
+    const result = createEvent(data.weekDay, data.week, data.openHour, data.closeHour);
+
+    //Expected
+    expect(result.title).toBe('[SOFKA U] Meeting Room');
+    expect(result.description).toBe('Mentoring and Practice');
+    expect(result.duration).toEqual([6, 'hour']);
 });
 
 test('Validation start date', () => {
-    //TODO: hacer las verificaciones
+    //Arrage
+
+    //At
+    
+
+    //Expected
 });
 
 test('Validation date', () => {
